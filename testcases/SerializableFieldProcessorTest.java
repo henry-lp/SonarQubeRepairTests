e624b35ea22ac989cdc36185cd7785d40d6f67dd
repairnator <noreply@github.com>
2020-05-29 21:39:16 +0200

    Proposal for patching SonarQube rule 1854

--- testcases/SerializableFieldProcessorTest.java
+++ testcases/SerializableFieldProcessorTest.java
@@ -1,16 +1,9 @@
 import java.io.Serializable;
-
-public class SerializableFieldProcessorTest implements Serializable
-{
+public class SerializableFieldProcessorTest implements Serializable {
     private Unser uns;// Noncompliant
-    public static void doNothing()
-    {
+
+
+    public static void doNothing() {
         int x = 500;
-        int y = x*x+x;
     }
-}
-
-class Unser
-{
-
 }
