691a7d9359103c769cee119fb161c1bd75926024
repairnator <noreply@github.com>
2020-05-30 12:41:00 +0200

    Proposal for patching SonarQube rule 2111

--- testcases/MultipleProcessors.java
+++ testcases/MultipleProcessors.java
@@ -1,17 +1,14 @@
 import java.math.BigDecimal;
-
 import java.util.concurrent.atomic.AtomicInteger;
-
 class MultipleProcessors {
+    void foo() {
+        BigDecimal bd2 = BigDecimal.valueOf(1.1);// Noncompliant by BigDecimalDoubleConstructorCheck
 
-	void foo() {
-		BigDecimal bd2 = new BigDecimal(1.1); // Noncompliant by BigDecimalDoubleConstructorCheck
+        float twoThirds = 2 / 3;// Noncompliant by CastArithmeticOperandCheck
 
-		float twoThirds = 2/3; // Noncompliant by CastArithmeticOperandCheck
+        AtomicInteger aInt1 = new AtomicInteger(0);
+        AtomicInteger aInt2 = new AtomicInteger(0);
+        isEqual = aInt1.equals(aInt2);// Noncompliant by EqualsOnAtomicClassCheck
 
-		AtomicInteger aInt1 = new AtomicInteger(0);
-		AtomicInteger aInt2 = new AtomicInteger(0);
-		isEqual = aInt1.equals(aInt2); // Noncompliant by EqualsOnAtomicClassCheck
-	}
-
+    }
 }
