7e4d560a17ec1bb62deb215b5de5a10cde56a286
repairnator <noreply@github.com>
2020-05-30 15:01:23 +0000

    Proposal for patching SonarQube rule 2204

--- testcases/MultipleProcessors.java
+++ testcases/MultipleProcessors.java
@@ -3,7 +3,7 @@ class MultipleProcessors {
     void foo() {
         AtomicInteger aInt1 = new AtomicInteger(0);
         AtomicInteger aInt2 = new AtomicInteger(0);
-        isEqual = aInt1.equals(aInt2);// Noncompliant by EqualsOnAtomicClassCheck
+        isEqual = aInt1.get() == aInt2.get();// Noncompliant by EqualsOnAtomicClassCheck
 
     }
 }
