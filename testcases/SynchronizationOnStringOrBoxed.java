8f4ff004af716d987ccaab0d6ae09dee68f13598
repairnator <noreply@github.com>
2020-05-30 12:41:00 +0200

    Proposal for patching SonarQube rule 1860

--- testcases/SynchronizationOnStringOrBoxed.java
+++ testcases/SynchronizationOnStringOrBoxed.java
@@ -1,51 +1,71 @@
 class SynchronizationOnStringOrBoxed {
-  
-  private final Boolean bLock = Boolean.FALSE;
-  private final Integer iLock = Integer.valueOf(0);
-  final String sLock = "LOCK";
-  private final Object oLock = new Object();
-  private final InnerClass i = new InnerClass();
+    private final Object iLockLegal = new Object();
 
-  class InnerClass {
-    public Boolean innerLock = Boolean.FALSE;
-    private SynchronizationOnStringOrBoxed outerClass = new SynchronizationOnStringOrBoxed();
+    private final Object bLockLegal = new Object();
 
-    public void method2() {
-      synchronized(this.outerClass.sLock) {  // Noncompliant
-        // ...
-      }
+    final Object sLockLegal = new Object();
+
+    private final Boolean bLock = Boolean.FALSE;
+
+    private final Integer iLock = Integer.valueOf(0);
+
+    final String sLock = "LOCK";
+
+    private final Object oLock = new Object();
+
+    private final InnerClass i = new InnerClass();
+
+    class InnerClass {
+        public Object innerLockLegal = new Object();
+
+        public Boolean innerLock = Boolean.FALSE;
+
+        private SynchronizationOnStringOrBoxed outerClass = new SynchronizationOnStringOrBoxed();
+
+        public void method2() {
+            synchronized(this.outerClass.sLockLegal) {
+                // Noncompliant
+                // ...
+            }
+        }
+
+        public Boolean getLock() {
+            return this.innerLock;
+        }
+
+        public Object getLockLegal() {
+            return this.innerLockLegal;
+        }
     }
 
-    public Boolean getLock() {
-      return this.innerLock;
+    void method1() {
+        synchronized(bLockLegal) {
+            // Noncompliant [[sc=18;ec=23]] {{Synchronize on a new "Object" instead.}}
+            // ...
+        }
+        synchronized(iLockLegal) {
+            // Noncompliant
+            // ...
+        }
+        synchronized(sLockLegal) {
+            // Noncompliant
+            // ...
+        }
+        synchronized(oLock) {
+            // ...
+        }
+        synchronized(i.getLockLegal()) {
+            // Noncompliant
+        }
     }
-  }
 
-  void method1() {
-    
-    synchronized(bLock) {  // Noncompliant [[sc=18;ec=23]] {{Synchronize on a new "Object" instead.}}
-      // ...
+    void method3() {
+        synchronized(sLockLegal) {
+            // Noncompliant
+            // ...
+        }
+        synchronized(i.getLockLegal()) {
+            // Noncompliant
+        }
     }
-    synchronized(iLock) {  // Noncompliant
-      // ...
-    }
-    synchronized(sLock) {  // Noncompliant
-      // ...
-    }
-    synchronized(oLock) { 
-      // ...
-    }
-    synchronized(i.getLock()) { // Noncompliant
-
-    }
-  }
-  
-  void method3() {
-    synchronized(sLock) {  // Noncompliant
-      // ...
-    }
-    synchronized(i.getLock()) { // Noncompliant
-
-    }
-  }
 }
