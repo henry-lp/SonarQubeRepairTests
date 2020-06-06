f9ae5e4bb53313f0319c80f3e17581d1da1a32a7
repairnator <noreply@github.com>
2020-06-06 15:48:53 +0200

    Proposal for patching Sorald rule 1854

--- testcases/ArrayHashCodeAndToString.java
+++ testcases/ArrayHashCodeAndToString.java
@@ -1,10 +1,10 @@
+
 // Test for rule s2116
 
 public class ArrayHashCodeAndToString {
 
 	// Tests from https://rules.sonarsource.com/java/type/Bug/RSPEC-2116
 	public static void main( String[] args ) {
-		String argStr = args.toString(); // Noncompliant
 		int argHash = args.hashCode(); // Noncompliant
 	}
 
