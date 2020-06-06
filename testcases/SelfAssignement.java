5d4bff1e0d6a71a51298a157757bf8a91a385435
repairnator <noreply@github.com>
2020-06-06 12:18:41 +0200

    Proposal for patching Sorald rule 1656

--- testcases/SelfAssignement.java
+++ testcases/SelfAssignement.java
@@ -1,11 +1,12 @@
 class SelfAssignement {
   int a,c = 0;
+
+	int c = 0;
   int[] b = {0};
   int h = 0;
   int[] g = 0;
   SelfAssignementCheckB checkB = new SelfAssignementCheckB();
   void method(int e,int h) {
-    a = a; // Noncompliant [[sc=7;ec=8]] {{Remove or correct this useless self-assignment.}}
     this.a = this.a; // Noncompliant
     b[0] = b[0]; // Noncompliant
     b[fun()] = b[fun()]; // Noncompliant
