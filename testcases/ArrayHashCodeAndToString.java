bb15158099f55e74a18c778c6812d896885a6a6a
repairnator <noreply@github.com>
2020-10-04 16:32:02 +0200

    Proposal for patching Sorald rule 2116

--- testcases/ArrayHashCodeAndToString.java
+++ testcases/ArrayHashCodeAndToString.java
@@ -1,10 +1,11 @@
+
 // Test for rule s2116
 
 public class ArrayHashCodeAndToString {
 
 	// Tests from https://rules.sonarsource.com/java/type/Bug/RSPEC-2116
 	public static void main( String[] args ) {
-		String argStr = args.toString(); // Noncompliant
+		String argStr = java.util.Arrays.toString(args); // Noncompliant
 		int argHash = args.hashCode(); // Noncompliant
 	}
 
